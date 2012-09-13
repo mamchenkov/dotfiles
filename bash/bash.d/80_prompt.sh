cc535a5ef50d4d9890da81c4b2909f229578e27d
Leonid Mamchenkov <leonid@mamchenkov.net>
2012-09-13 03:06:00 +0300

    256 color support in bash prompt

--- bash/bash.d/80_prompt.sh
+++ bash/bash.d/80_prompt.sh
@@ -1,6 +1,7 @@
 #
 # Build shell prompt
 # 
+# Most of the code is from: https://gist.github.com/293517
 
 # Print flag character if current git directory is locally modified
 # 
@@ -10,38 +11,67 @@ function __git_dirty {
 	[ $? == 1 ] && echo "M"
 }
 
+function smart_pwd {
+    local pwdmaxlen=25
+    local trunc_symbol=".."
+    local dir=${PWD##*/}
+    local tmp=""
+    pwdmaxlen=$(( ( pwdmaxlen < ${#dir} ) ? ${#dir} : pwdmaxlen ))
+    NEW_PWD=${PWD/#$HOME/\~}
+    local pwdoffset=$(( ${#NEW_PWD} - pwdmaxlen ))
+    if [ ${pwdoffset} -gt "0" ]
+    then
+        tmp=${NEW_PWD:$pwdoffset:$pwdmaxlen}
+        tmp=${trunc_symbol}/${tmp#*/}
+        if [ "${#tmp}" -lt "${#NEW_PWD}" ]; then
+            NEW_PWD=$tmp
+        fi
+    fi
+}
 
-if [ "$EUID" -eq "0" ]
-then
-	# Root user gets red background
-	RED="\[\033[41;1;31m\]"
-	CYAN="\[\033[41;1;36m\]"
-	MAGENTA="\[\033[41;1;35m\]"
-	BLUE="\[\033[41;34m\]"
-	YELLOW="\[\033[41;1;33m\]"
-	GREEN="\[\033[41;1;32m\]"
-	BLACK_ON_WHITE="\[\033[0m\]"
-else
-	# Regular user gets blue background
-	RED="\[\033[44;1;31m\]"
-	CYAN="\[\033[44;1;36m\]"
-	MAGENTA="\[\033[44;1;35m\]"
-	BLUE="\[\033[44;34m\]"
-	YELLOW="\[\033[44;1;33m\]"
-	GREEN="\[\033[44;1;32m\]"
-	BLACK_ON_WHITE="\[\033[0m\]"
-fi
+function boldtext {
+    echo "\\[\\033[1m\\]"$1"\\[\\033[0m\\]"
+}
 
-case $TERM in
-	xterm*|rxvt*)
-		TITLEBAR="\[\033]0;\u@\H:\w\007\]\n"
-		;;
-	*)
-		TITLEBAR=""
-		;;
+function bgcolor {
+    echo "\\[\\033[48;5;"$1"m\\]"
+}
+
+function fgcolor {
+    echo "\\[\\033[38;5;"$1"m\\]"
+}
+
+function resetcolor {
+    echo "\\[\\e[0m\\]"
+}
+
+function fancyprompt {
+    PROMPT_COMMAND="smart_pwd"
+
+	if [ "$EUID" -eq "0" ]
+	then
+		PS1="$(bgcolor 196)$(fgcolor 114)[$(fgcolor 117)\t$(fgcolor 114)][$(fgcolor 190)\u$(fgcolor 114)@$(fgcolor 190)\h$(fgcolor 114):$(fgcolor 86)\w$(fgcolor 190)\$(__git_ps1 \" (%s)\")$(fgcolor 196)\$(__git_dirty)$(fgcolor 114)]#$(resetcolor) "
+	else
+		PS1="$(bgcolor 17)$(fgcolor 114)[$(fgcolor 117)\t$(fgcolor 114)][$(fgcolor 190)\u$(fgcolor 114)@$(fgcolor 190)\h$(fgcolor 114):$(fgcolor 86)\w$(fgcolor 190)\$(__git_ps1 \" (%s)\")$(fgcolor 196)\$(__git_dirty)$(fgcolor 114)]\$$(resetcolor) "
+	fi
+}
+
+function dullprompt {
+    PROMPT_COMMAND=""
+	if [ "$EUID" -eq "0" ]
+	then
+		PS1="[\t][\u@\h:\w\$(__git_ps1 \" (%s)\")\$(__git_dirty)]# "
+	else
+		PS1="[\t][\u@\h:\w\$(__git_ps1 \" (%s)\")\$(__git_dirty)]\$ "
+	fi
+}
+
+case "$TERM" in
+xterm-color|xterm-256color|rxvt*|screen-256color)
+        fancyprompt
+    ;;
+*)
+        dullprompt
+    ;;
 esac
 
-export PROMPT_DIRTRIM=3 # Limit trailing directory components
-
-PS1="${TITLEBAR}${CYAN}[\t][${MAGENTA}\u${CYAN}@${YELLOW}\h${CYAN}:${GREEN}\w${YELLOW}\$(__git_ps1 \" (%s)\")${RED}\$(__git_dirty)${CYAN}]${BLACK_ON_WHITE}\$ "
-PS2="${CYAN}[\t][$MAGENTA\u$CYAN@$YELLOW\h${CYAN}:$GREEN\W$CYAN]${BLACK_ON_WHITE}> "
