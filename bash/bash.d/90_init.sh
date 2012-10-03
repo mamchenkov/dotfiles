7dcb76385f20de0fcfc633f847c61af9b0052f38
Leonid Mamchenkov <leonid@mamchenkov.net>
2012-10-03 03:30:16 +0300

    Dump/load directory bookmarks on login/logout

--- bash/bash.d/90_init.sh
+++ bash/bash.d/90_init.sh
@@ -1,6 +1,13 @@
 # 
 # Last bits
 # 
+
+# Show host information
 have whereami && whereami
+
+# Show thought of the day
 have fortune && echo Thought of the day: && fortune -s && echo
+
+# Load directory bookmarks
+have mload && mload
 		
