f9298976748283dcd2ab65be54febd0271259bab
Leonid Mamchenkov <leonid@mamchenkov.net>
2015-10-13 11:52:34 +0300

    Handle root home correctly

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,14 @@
 node default {
-	include bash
-	include vim
-	include git
-	include fonts
+
+	if $id == 'root' {
+		$home = '/root'
+	}
+	else {
+		$home = "/home/$id"
+	}
+
+	class { 'bash': home => $home }
+	class { 'vim': home => $home }
+	class { 'git': home => $home }
+	class { 'fonts': home => $home }
 }
