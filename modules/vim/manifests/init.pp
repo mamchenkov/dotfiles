eed2fefa1a903fe6966588cd7941124620bb2c6f
Leonid Mamchenkov <leonid@mamchenkov.net>
2015-10-13 11:08:10 +0300

    Handle vim with vim module (all but bundles)

--- modules/vim/manifests/init.pp
+++ modules/vim/manifests/init.pp
@@ -2,6 +2,7 @@ class vim (String $home = '/home/leonid') {
 
 	if $id == 'root' {
 		$packages = [
+			'ctags',
 			'vim-enhanced',
 		]
 
@@ -10,6 +11,48 @@ class vim (String $home = '/home/leonid') {
 		}
 	} else {
 		warning("Skipping package installation for non-root user")
+	}
+
+	file { "$home/.vim":
+		ensure => 'directory',
+	}
+
+	file { "$home/.vim/autoload":
+		ensure => 'directory',
+		source => 'puppet:///modules/vim/vim/autoload',
+		recurse => true,
+		purge => true,
+		mode => 'a+rX,ug+w,o-w',
+		require => File["$home/.vim"],
+	}
+
+	file { "$home/.vim/bundle":
+		ensure => 'directory',
+		recurse => false,
+		purge => false,
+		mode => 'a+rX,ug+w,o-w',
+		require => File["$home/.vim"],
+	}
+
+	file { "$home/.vim/sessions":
+		ensure => 'directory',
+		recurse => false,
+		purge => false,
+		mode => 'a+rX,ug+w,o-w',
+		require => File["$home/.vim"],
+	}
+
+	file { "$home/.vim/undodir":
+		ensure => 'directory',
+		recurse => false,
+		purge => false,
+		mode => 'a+rX,ug+w,o-w',
+		require => File["$home/.vim"],
+	}
+
+	file { "$home/.vimrc":
+		ensure => 'present',
+		source => 'puppet:///modules/vim/vimrc',
 	}
 
 }
